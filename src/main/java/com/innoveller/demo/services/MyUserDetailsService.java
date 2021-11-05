daa437206733aabf62b6e46b4741a93babfedb61
Sindhu <oceansindhu200@gmail.com>
2021-11-05 07:04:16 +0630

    remove changes

--- src/main/java/com/innoveller/demo/services/MyUserDetailsService.java
+++ src/main/java/com/innoveller/demo/services/MyUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.innoveller.demo.services;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -7,11 +9,41 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        return new User("Judy", "12345", new ArrayList<>());
+//        List<GrantedAuthority> adminAuthority = new ArrayList<>();
+//        adminAuthority.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+//
+//        List<GrantedAuthority> userAuthority = new ArrayList<>();
+//        userAuthority.add(new SimpleGrantedAuthority("ROLE_USER"));
+//
+//        User adminUser = new User("Judy", "12345", adminAuthority);
+//        User staffUser = new User("Sindhu", "45678", userAuthority);
+
+        UserDetails adminUser = User.withUsername("Judy")
+                .password("12345")
+                .roles("ADMIN")
+                .build();
+        UserDetails staffUser = User.withUsername("Sindhu")
+                .password("12345")
+                .roles("USER")
+                .build();
+
+
+        List<UserDetails> userList = new ArrayList<>();
+        userList.add(adminUser);
+        userList.add(staffUser);
+
+        Optional<UserDetails> userOptional = userList.stream().filter(user -> user.getUsername().equals(userName)).findFirst();
+        if(userOptional.isPresent()) {
+            return userOptional.get();
+        } else {
+            throw new UsernameNotFoundException("User does not exit");
+        }
     }
 }
